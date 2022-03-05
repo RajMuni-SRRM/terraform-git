757b9142b08c4c4a81d07f50b62556dd78a207ea
Rohiniraj <67454746+RajMuni-SRRM@users.noreply.github.com>
2022-03-05 22:37:27 +0530

    Update main.tf

--- instance-create/main.tf
+++ instance-create/main.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 */
+  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 provider "aws" {
     region = "eu-west-2"
     version = "~> 1.0"
